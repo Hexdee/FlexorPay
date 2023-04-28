b47a938b30840239702a9641caf72a75a31a684b
Hexdee <lukmansaheed79@gmail.com>
2023-04-28 12:17:20 +0100

    Install hardhat

---
+++ contract/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+module.exports = {
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {
+    },
+    mumbai: {
+      url: process.env.QUICKNODE_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+    }
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
