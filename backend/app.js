abe309cc7912f8a3a79a5a276305c4935b2d6282
Hexdee <lukmansaheed79@gmail.com>
2023-05-14 19:00:07 +0100

    Create FlexorPay backend

---
+++ backend/app.js
@@ -0,0 +1,204 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// const { randomUUID } = require('crypto');
+const path = require("path");
+const auth = require("./middleware/auth");
+const cors = require("cors");
+const ethers = require('ethers');
+const flexorpayAbi = require("./flexorpay.json").abi;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+// importing user context
+const User = require("./model/user");
+const Transaction = require("./model/transaction");
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Welcome to flexorpay");
+})
+
+app.get("/:username", async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username });
+  if (user) {
+    res.status(200);
+    res.render('payment', { merchant: username });
+  } else {
+    res.status(404);
+    res.send("User not found!");
+  }
+});
+
+// return a user details
+app.get("/user/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username })
+    let { first_name, last_name, email, balance } = user;
+    if (!balance) {
+      balance = 0;
+    }
+    res.status(200);
+    res.send({ first_name, last_name, email, username, balance })
+  } catch (err) {
+    res.status(404);
+    res.send("user not found");
+  }
+});
+
+app.post("/auth", auth, async (req, res) => {
+  try {
+    const { email } = req.user;
+    const user = await User.findOne({ email });
+    const { first_name, last_name, username, balance, transactions } = user;
+    res.status(200);
+    res.send({ first_name, last_name, email, username, email, balance, transactions });
+  } catch (err) {
+    res.status(400);
+    res.send("Bad request");
+  }
+})
+
+
+// Register
+app.post("/register", async (req, res) => {
+
+  // Our register logic starts here
+  try {
+    // Get user input
+    const { first_name, last_name, email, username, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name && username)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login!");
+    }
+
+    // Check if username is taken
+    const oldUsername = await User.findOne({ username });
+
+    if (oldUsername) {
+      return res.status(409).send("Username Already Taken!");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      balance: 0,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      username: username.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json({ first_name, last_name, balance: 0, email, username, token });
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+
+
+// Login
+app.post("/login", async (req, res) => {
+
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "3d",
+        }
+      );
+
+      // save user token
+      user.token = token;
+      const { first_name, last_name, balance, transactions } = user;
+      // user
+      res.status(200).json({ first_name, last_name, email, balance, token, transactions });
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+
+// Listen to smart contract for payment and update user balance
+const flexorpayAddress = "0xd086dAB59F3d183b77c14E6FbbacC421adCD1634";
+const provider = new ethers.providers.JsonRpcProvider(`https://rpc-mumbai.maticvigil.com/v1/${process.env.MATIC_VIGIL_API_KEY}`);
+
+const contract = new ethers.Contract(flexorpayAddress, flexorpayAbi, provider);
+contract.on("paymentSuccessful", async (amount, payer, payer_address, merchant, description) => {
+  try {
+    const value = ethers.utils.formatEther(amount);
+    console.log({ merchant })
+
+    const user = await User.findOne({ username: merchant });
+    let balance = user.balance || 0;
+    balance = Number(balance) + Number(value);
+    const newTransaction = await Transaction.create({
+      type: "payment",
+      payer,
+      payer_address,
+      amount: Number(ethers.utils.formatEther(amount)),
+      merchant,
+      description,
+      status: "confirm",
+    })
+    const transactions = user.transactions;
+    transactions.unshift(newTransaction);
+    await User.updateOne({ username: merchant }, { balance, transactions });
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+
+
+module.exports = app;
---
+++ backend/public/script.js
@@ -0,0 +1,261 @@
+var flexorpayAddress = "0xd086dAB59F3d183b77c14E6FbbacC421adCD1634";
+var flexorpayAbi = [{ "inputs": [{ "internalType": "address", "name": "_token", "type": "address" }, { "internalType": "uint256", "name": "_amountInUSD", "type": "uint256" }, { "internalType": "string", "name": "_name", "type": "string" }, { "internalType": "string", "name": "_description", "type": "string" }, { "internalType": "string", "name": "_merchant", "type": "string" }], "name": "makePayment", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amountInUSD", "type": "uint256" }, { "internalType": "address", "name": "_token", "type": "address" }], "name": "requiredTokenAmount", "outputs": [{ "internalType": "uint256", "name": "_tokenAmount", "type": "uint256" }], "stateMutability": "view", "type": "function" }];
+var erc20Abi = [{ "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }];
+var address0 = "0x0000000000000000000000000000000000000000";
+var weth = "0x5B67676a984807a212b1c59eBFc9B3568a474F0a"
+var stableCoinDecimals = 1e6;
+
+// https://docs.walletconnect.com/quick-start/dapps/web3-provider
+var provider = new WalletConnectProvider.default({
+    rpc: {
+        //1: "https://cloudflare-eth.com/", // https://ethereumnodes.com/
+        //137: "https://polygon-rpc.com/", // https://docs.polygon.technology/docs/develop/network-details/network/
+        80001: "https://rpc-mumbai.maticvigil.com/",
+        // ...
+
+    },
+    // bridge: 'https://bridge.walletconnect.org',
+});
+
+document.getElementById("token").addEventListener("change", async (e) => {
+    //
+    const requiredTokenValue = 1 * stableCoinDecimals;
+    const tokenAddress = e.target.value;
+    const tokenContract = await contract(erc20Abi, tokenAddress);
+    try {
+        const allowance = Number(await tokenContract.methods.allowance(account, flexorpayAddress).call());
+        const payBtn = document.getElementById("pay");
+        const approveBtn = document.getElementById("approve");
+        if (allowance < requiredTokenValue) {
+            payBtn.style.display = "none";
+            approveBtn.style.display = "block"
+        } else {
+            payBtn.style.display = "block";
+            approveBtn.style.display = "none"
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+const setFrom = () => {
+    document.getElementById("from").value = account;
+}
+
+const getTokens = async () => {
+    try {
+        const url = new URL(`https://deep-index.moralis.io/api/v2/${account}/erc20?chain=mumbai`);
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-Key': '4QdwNluHelpTw9qmoAXTsaodpYXP1E1cpdrRmqbTGf9sPhO9hBFPrRydJxkl5TPP'
+            }
+        })
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const setTokens = async (tokens) => {
+    document.getElementById("token").innerHTML =
+        tokens.map((token) => {
+            return `<option value="${token.token_address}">${token.symbol}</option>`
+            // }
+        })
+}
+
+const setPortfolio = async (tokens) => {
+    let tokensElement = "";
+    console.log({ tokens })
+    tokens.map((tkn) => {
+        let balance = tkn.balance / 10 ** tkn.decimals;
+        if (balance < 0.01) {
+            balance = balance.toFixed(4)
+        } else {
+            balance = balance.toFixed(2)
+        }
+        tokensElement += `<div class="token">
+            <div class="token-img"></div>
+            <p><strong>${(balance)} ${tkn.symbol}</strong></p>
+            </div>`
+    })
+    document.querySelector(".tokens").innerHTML = tokensElement;
+}
+
+const init = async (account) => {
+    if (account) {
+        window.account = account;
+        const matic = {
+            symbol: "MATIC",
+            token_address: address0,
+            balance: Number(await window.ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] })),
+            decimals: 18
+        }
+
+        const tokens = [matic, ...(await getTokens(account))];
+        setFrom();
+        setTokens(tokens);
+        setPortfolio(tokens);
+        // document.querySelector(".connect").style.display = "none";
+    }
+}
+
+const connect = async () => {
+    // if(window.account) {
+    //     disconnect();
+    //     return;
+    // }
+    if (window.ethereum) {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        web3 = new Web3(window.ethereum);
+        window.w3 = web3;
+        try {
+            // check if the chain to connect to is installed
+            await window.ethereum.request({
+                method: 'wallet_switchEthereumChain',
+                params: [{ chainId: '0x13881' }], // chainId must be in hexadecimal numbers
+            });
+        } catch (error) {
+            // This error code indicates that the chain has not been added to MetaMask
+            // if it is not, then install it into the user MetaMask
+            if (error.code === 4902) {
+                try {
+                    await window.ethereum.request({
+                        method: 'wallet_addEthereumChain',
+                        params: [
+                            {
+                                chainId: "0x13881",
+                                chainName: "Polygon Testnet",
+                                nativeCurrency: { name: "Polygon Token", symbol: "MATIC", decimals: 18 },
+                                rpcUrls: ["https://matic-mumbai.chainstacklabs.com"],
+                                blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+                                iconUrls: ["future"],
+                            }
+                        ],
+                    });
+                } catch (addError) {
+                    console.error(addError);
+                    window.alert('failed to add network to metamask');
+                    return;
+                }
+            } else {
+                console.log(error);
+                window.alert('failed to switch network to polygon testnet')
+                return;
+            }
+            window.ethereum.on('accountsChanged', function (accounts) {
+                init(accounts[0]);
+            });
+        }
+        init(account);
+    } else {
+        if (provider.connected) {
+            await provider.disconnect();
+        }
+        await provider.enable();
+
+        //  Create Web3 instance
+        const web3 = new Web3(provider);
+        window.w3 = web3;
+        const accounts = await web3.eth.getAccounts(); // get all connected accounts
+        const account = accounts[0];
+        console.log(await web3.eth.getChainId())
+        init(account);
+        provider.on("accountsChanged", (accounts) => {
+            init(accounts[0])
+        });
+    }
+    // document.getElementById("connect").innerText = "Disconnect ";
+}
+
+// const sign = async (msg) => {
+//     if (w3) {
+//     return await w3.eth.personal.sign(msg, account);
+//     } else {
+//         return false
+//     }
+// }
+
+const contract = async (abi, address) => {
+    if (w3) {
+        return new w3.eth.Contract(abi, address)
+    } else {
+        return false
+    }
+}
+
+var disconnect = async () => {
+    if (provider.connected) {
+        // Close provider session
+        await provider.disconnect();
+    }
+}
+
+const approveToken = async () => {
+    const tokenAddress = document.getElementById("token").value;
+    const payBtn = document.getElementById("pay");
+    const approveBtn = document.getElementById("approve");
+    try {
+        approveBtn.innerText = "Loading...";
+        const tokenContract = await contract(erc20Abi, tokenAddress);
+        const tx = await tokenContract.methods.approve(flexorpayAddress, "0xffffffffffffffffffffffffffffffff").send({ from: account });
+        await tx.wait;
+        payBtn.style.display = "block";
+        approveBtn.style.display = "none";
+    }
+    catch (err) {
+        console.log(err);
+        alert("An error occured");
+    } finally {
+        approveBtn.innerText = "Approve";
+    }
+}
+
+const requiredTokenAmount = async (token, amountInUSD) => {
+    const flexorpay = await contract(flexorpayAbi, flexorpayAddress);
+    const amount = await flexorpay.methods.requiredTokenAmount(amountInUSD * stableCoinDecimals, token).call();
+    return amount;
+}
+
+const makePayment = async () => {
+    const token = document.getElementById("token").value;
+    const amount = document.getElementById("amount").value;
+    const merchant = document.getElementById("to").value;
+    const name = document.getElementById("name").value;
+    const desc = document.getElementById("desc").value;
+    const payBtn = document.getElementById("pay");
+
+    if (token && name && amount && desc) {
+        try {
+            payBtn.innerText = "Loading..."
+            const flexorpay = await contract(flexorpayAbi, flexorpayAddress);
+            if (token != address0) {
+                const tx = await flexorpay.methods.makePayment(token, amount * stableCoinDecimals, name, desc, merchant).send({ from: account });
+                await tx.wait;
+            } else {
+                const requireAmount = await requiredTokenAmount(weth, amount);
+                const tx = await flexorpay.methods.makePayment(token, amount * stableCoinDecimals, name, desc, merchant).send({ from: account, value: requireAmount });
+            }
+            // alert("Payment successful");
+            payBtn.innerText = "Payment successful";
+            payBtn.onclick = "";
+            payBtn.style.backgroundColor = "green";
+        } catch (err) {
+            console.log(err);
+            alert("An error occured");
+            payBtn.innerText = "Pay with FlexorPay";
+        }
+    } else if (!amount) {
+        alert("Enter amount to be paid");
+    } else if (!name) {
+        alert("Enter your name")
+    } else if (!desc) {
+        alert("Enter description");
+    }
+    else {
+        alert("Please select a token");
+    }
+}
